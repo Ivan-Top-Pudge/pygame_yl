b9e81eb5d8cf8c4406a4fd0994c8407b95865f21
Ivan-Top-Pudge <vananufr1ev@yandex.ru>
2023-01-09 11:19:27 +0400

    Есть карта и есть танк, который ездит.

---
+++ main.py
@@ -0,0 +1,106 @@
+import os
+import sys
+import pygame
+import pytmx
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('src', name)
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
+class Map:
+    def __init__(self):
+        self.map = pytmx.load_pygame("map2.tmx")
+        self.height = self.map.height
+        self.width = self.map.width
+        self.tile_size = self.map.tilewidth
+
+    def render(self, screen):
+        for y in range(self.height):
+            for x in range(self.width):
+                image = self.map.get_tile_image(x, y, 0)
+                screen.blit(image, (x * self.tile_size, y * self.tile_size))
+
+    def get_tile_id(self, position):
+        return self.map.tiledgidmap[self.map.get_tile_gid(*position, 0)]
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__(player_group, all_sprites)
+        self.images = (pygame.transform.rotate(player_image, 90),
+                       pygame.transform.rotate(player_image, 180),
+                       pygame.transform.rotate(player_image, 270),
+                       pygame.transform.rotate(player_image, 360))
+        self.image = self.images[0]
+        self.rect = self.image.get_rect().move(
+            32 * pos_x + 15, 32 * pos_y + 5)
+
+    def move(self, act):
+        if act.key == pygame.K_LEFT:
+            self.image = self.images[0]
+            self.rect.x -= 10
+        elif act.key == pygame.K_RIGHT:
+            self.image = self.images[2]
+            self.rect.x += 10
+        elif act.key == pygame.K_UP:
+            self.image = self.images[3]
+            self.rect.y -= 10
+        elif act.key == pygame.K_DOWN:
+            self.image = self.images[1]
+            self.rect.y += 10
+
+
+class Camera:
+    def __init__(self):
+        self.dx = 0
+        self.dy = 0
+
+    def apply(self, obj):
+        obj.rect.x += self.dx
+        obj.rect.y += self.dy
+
+    def update(self, target):
+        self.dx = -(target.rect.x + target.rect.w // 2 - width // 2)
+        self.dy = -(target.rect.y + target.rect.h // 2 - height // 2)
+
+
+if __name__ == '__main__':
+    pygame.init()
+    pygame.display.set_caption("Ануфриев copyright all rights reserved")
+    size = width, height = 40 * 32, 35 * 32
+    screen = pygame.display.set_mode(size)
+    running = True
+    clock = pygame.time.Clock()
+    karta = Map()
+    all_sprites = pygame.sprite.Group()
+    player_group = pygame.sprite.Group()
+    player_image = load_image('sprites/tank_test.png', -1)
+    player = Player(1, 1)
+    camera = Camera()
+    while running:
+        screen.fill('black')
+        screen.fill('black')
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.KEYDOWN:
+                player.move(event)
+        karta.render(screen)
+        all_sprites.draw(screen)
+        all_sprites.update()
+        pygame.display.flip()
+        clock.tick(60)
+    pygame.quit()
---
+++ tmw_desert_spacing.tsx
@@ -0,0 +1,36 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="tmw_desert_spacing" tilewidth="32" tileheight="32" spacing="1" margin="1" tilecount="48" columns="8">
+ <image source="../../Program Files/Tiled/examples/tmw_desert_spacing.png" width="265" height="199"/>
+ <wangsets>
+  <wangset name="Безымянный набор" type="mixed" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+   <wangcolor name="" color="#00ff00" tile="-1" probability="1"/>
+   <wangtile tileid="0" wangid="0,0,2,2,2,0,0,0"/>
+   <wangtile tileid="1" wangid="0,0,2,2,2,2,2,0"/>
+   <wangtile tileid="2" wangid="0,0,0,0,2,2,2,0"/>
+   <wangtile tileid="8" wangid="2,2,2,2,2,0,0,0"/>
+   <wangtile tileid="9" wangid="2,2,2,2,2,2,2,2"/>
+   <wangtile tileid="10" wangid="2,0,0,0,2,2,2,2"/>
+   <wangtile tileid="16" wangid="2,2,2,0,0,0,0,0"/>
+   <wangtile tileid="17" wangid="2,2,2,0,0,0,2,2"/>
+   <wangtile tileid="18" wangid="2,0,0,0,0,0,2,2"/>
+   <wangtile tileid="19" wangid="2,2,2,0,2,2,2,2"/>
+   <wangtile tileid="20" wangid="2,2,2,2,2,0,2,2"/>
+   <wangtile tileid="24" wangid="0,0,1,1,1,0,0,0"/>
+   <wangtile tileid="25" wangid="0,0,1,1,1,1,1,0"/>
+   <wangtile tileid="26" wangid="0,0,0,0,1,1,1,0"/>
+   <wangtile tileid="27" wangid="2,0,2,2,2,2,2,2"/>
+   <wangtile tileid="28" wangid="2,2,2,2,2,2,2,0"/>
+   <wangtile tileid="32" wangid="1,1,1,1,1,0,0,0"/>
+   <wangtile tileid="33" wangid="1,1,1,1,1,1,1,1"/>
+   <wangtile tileid="34" wangid="1,0,0,0,1,1,1,1"/>
+   <wangtile tileid="35" wangid="1,1,1,0,1,1,1,1"/>
+   <wangtile tileid="36" wangid="1,1,1,1,1,0,1,1"/>
+   <wangtile tileid="40" wangid="1,1,1,0,0,0,0,0"/>
+   <wangtile tileid="41" wangid="1,1,1,0,0,0,1,1"/>
+   <wangtile tileid="42" wangid="1,0,0,0,0,0,1,1"/>
+   <wangtile tileid="43" wangid="1,0,1,1,1,1,1,1"/>
+   <wangtile tileid="44" wangid="1,1,1,1,1,1,1,0"/>
+  </wangset>
+ </wangsets>
+</tileset>
