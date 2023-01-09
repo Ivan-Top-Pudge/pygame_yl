287e10b72480f37e79a2826fe9ac88d88c857da7
Ivan-Top-Pudge <vananufr1ev@yandex.ru>
2023-01-09 18:03:27 +0400

    Сделана стрельба, изменена техника поворота

--- main.py
+++ main.py
@@ -58,28 +58,55 @@ class Player(pygame.sprite.Sprite):
                        pygame.transform.rotate(player_image, 180),
                        pygame.transform.rotate(player_image, 270),
                        pygame.transform.rotate(player_image, 360))
-        self.image = self.images[0]
+        self.pos = 0
+        self.image = self.images[self.pos]
         self.rect = self.image.get_rect().move(
             32 * pos_x + 15, 32 * pos_y + 5)
 
     def move(self, act):
         if act.key == pygame.K_LEFT:
-            self.image = self.images[0]
+            self.pos = 0
             self.rect.x -= 10
         elif act.key == pygame.K_RIGHT:
-            self.image = self.images[2]
+            self.pos = 2
             self.rect.x += 10
         elif act.key == pygame.K_UP:
-            self.image = self.images[3]
+            self.pos = 3
             self.rect.y -= 10
         elif act.key == pygame.K_DOWN:
-            self.image = self.images[1]
+            self.pos = 1
             self.rect.y += 10
+        self.image = self.images[self.pos]
+
+    def shoot(self):
+        Bullet(self.rect.x, self.rect.y, self.pos)
 
 
 class Bullet(pygame.sprite.Sprite):
     def __init__(self, pos_x, pos_y, direction):
         super().__init__(bullets, all_sprites)
+        self.image = bullet_images[direction]
+        self.speedx, self.speedy = 0, 0
+        x, y = 0, 0
+        if direction == 0:
+            x, y = pos_x - 30, pos_y + 30
+            self.speedx -= 10
+        elif direction == 1:
+            x, y = pos_x + 35, pos_y + 70
+            self.speedy += 10
+        elif direction == 2:
+            x, y = pos_x + 70, pos_y + 30
+            self.speedx += 10
+        elif direction == 3:
+            x, y = pos_x + 33, pos_y - 30
+            self.speedy -= 10
+        self.rect = self.image.get_rect().move(x, y)
+
+    def update(self):
+        self.rect.x += self.speedx
+        self.rect.y += self.speedy
+        if self.rect.x < 0 or self.rect.x > 40 * 32 or self.rect.y < 0 or self.rect.y > 35 * 32:
+            self.kill()
 
 
 if __name__ == '__main__':
@@ -95,17 +122,25 @@ if __name__ == '__main__':
     walls = pygame.sprite.Group()
     bullets = pygame.sprite.Group()
     player_image = load_image('sprites/tank_test.png', -1)
-    player = Player(1, 1)
+    bullet_image = load_image("sprites/bullet.png", -1)
+    bullet_images = (pygame.transform.rotate(bullet_image, 90),
+                       pygame.transform.rotate(bullet_image, 180),
+                       pygame.transform.rotate(bullet_image, 270),
+                       pygame.transform.rotate(bullet_image, 360))
+    player = Player(20, 15)
     karta.generate_groups()
     while running:
         screen.fill('black')
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                player.shoot()
             elif event.type == pygame.KEYDOWN:
                 player.move(event)
         walls.draw(screen)
         player_group.draw(screen)
+        bullets.draw(screen)
         all_sprites.update()
         pygame.display.flip()
         clock.tick(60)
